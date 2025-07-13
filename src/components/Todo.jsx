7a1f524f3a38a4bac7b99330fb50adaddbe26f19
MOSTAFA <mhrmo75@gmail.com>
2025-07-13 13:08:10 +0330

    reforms & style text.name

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -9,7 +9,7 @@ export default function Todo() {
         e.preventDefault()
     if(values.trim()){
      const newItem={
-        id:Date.now(),
+        id:Date.now() + Math.random(),
         name:values,
         txt:'Hello',
         message:'Welcome to my website.',
@@ -50,7 +50,9 @@ const textColor=(id)=>{
    {items.map((item)=>(
   <div key={item.id} className={`todo flex justify-around items-center gap-[50px] w-[40%] py-2 rounded-lg hover:scale-[1.02] transition-scale duration-[.3s,.3s] ease-in-out ${item.bgChange ? 'bg-blue-500' : 'bg-emerald-800'}`}>
     <div className={`flex gap-2 w-100 text-[20px] ${item.textChange ? 'text-black' : 'text-white'}`}>
-        {item.txt} {item.name} {item.message}
+        {item.txt}
+         <p className={`font-bold ${item.textChange ?'text-white':'text-black'}`}>{item.name}</p>
+        {item.message}
     </div>
     <div className='flex gap-5'>
       <button className='bg-green-500 hover:bg-green-700 transition-all duration-300 w-12 h-10 rounded-lg cursor-pointer text-white text-[12px]' onClick={() => bgColor(item.id)}>Bg Change</button>
